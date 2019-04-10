65232d5b39ec4659aa8c26d40e3fcfa42b149350
Aiyu Liu <mqt615@alumni.ku.dk>
2019-04-10 14:29:58 +0100

    little code cleanup and trying to debug 5 extra degrees

--- Romi/Romi.ino
+++ Romi/Romi.ino
@@ -59,7 +59,6 @@ PID           HeadingControl( 1, 0, 0.001 );
 PID           ForwardHeadingControl( 5, 0, 0 );
 
 
-
 //PID           RightSpeedControl( 3.5, 20.9, 0.04 );
 Mapper        Map; //Class for representing the map
 
@@ -90,21 +89,23 @@ bool use_speed_controller = true;
 float left_speed_demand = 0;
 float right_speed_demand = 0;
 
+// State machine
+int STATE;
 
-float angle;
-
+// Going forward
 int COUNT = COUNTS_PER_MM * 72;
 int count = COUNT;
-int STATE;
+
+// Determine direction
 char SMALLEST;
 int FLAG = FORWARD;
 float dir;
-int condition_walk = 0;
 int FLAG_SMALLEST;
 
+// Going from 180 to -90
 int MINUS90;
 
-
+// For reading the surrounding map
 char n_val;
 char s_val;
 char e_val;
@@ -157,7 +158,6 @@ void setup()
   // Set the random seed for the random number generator
   // from A0, which should itself be quite random.
   randomSeed(analogRead(A0));
-
   
   // Initialise Serial communication
   Serial.begin( BAUD_RATE );
@@ -170,33 +170,21 @@ void setup()
   // !!! A second button press will erase the map !!!
   ButtonB.waitForButton();  
 
- 
-
   Map.printMap();
 
   // Watch for second button press, then begin autonomous mode.
   ButtonB.waitForButton();  
 
-  Serial.println("Map Erased - Mapping Started");
   Map.resetMap();
+  Serial.println("Map Erased - Mapping Started");
+
+  //Draw map
   Map.initMap();
 
-  // Your extra setup code is best placed here:
-  // ...
-  // ...
-  // but not after the following:
-
-  // Because code flow has been blocked, we need to reset the
-  // last_time variable of the PIDs, otherwise we update the
-  // PID with a large time elapsed since the class was 
-  // initialised, which will cause a big intergral term.
-  // If you don't do this, you'll see the Romi accelerate away
-  // very fast!
   LeftSpeedControl.reset();
   RightSpeedControl.reset();
   left_speed_demand = 0;
   right_speed_demand = 0;
-  
   
 }
 
@@ -217,17 +205,15 @@ void loop() {
 
   doMapping();
 
-  //Serial.println(Pose.getThetaDegrees());
-  //Serial.println(right_encoder_count);
+  Serial.print("LOOP THETA: ");
+  Serial.println(Pose.getThetaDegrees());
+  
   wavefront();
-  // Rotation
   
   delay(2);
 }
 
 void wavefront(){
-
-  
   switch(STATE){
     case 0: forward();
             break;
@@ -241,38 +227,39 @@ void wavefront(){
 }
 
 void forward(){
+  Serial.print(", right counts: ");
+  Serial.print(right_encoder_count);
+  Serial.print(", left counts: ");
+  Serial.println(left_encoder_count);
   int condition = (count - right_encoder_count) < 0;
   float forward_heading_output  = ForwardHeadingControl.update(dir, Pose.getThetaDegrees());
-  
-  Serial.print(", Degreesssssss: ");
-  Serial.println(Pose.getThetaDegrees() );
 
   if(condition){
-    // Read map
     stop_speed();
-
-    
     STATE = WALK;
+    
   }
   else{
-    int demand = 10;
-      Serial.print("Direction: ");
-  Serial.print( dir );
-  Serial.print(", Degree: ");
-  Serial.print(Pose.getThetaDegrees() );
-  Serial.print(", output:  ");
-  Serial.print( forward_heading_output );
+    int demand = 12;
+    Serial.print("Direction: ");
+    Serial.print( dir );
+    Serial.print(", Degree: ");
+    Serial.print(Pose.getThetaDegrees() );
+    Serial.print(", output:  ");
+    Serial.print( forward_heading_output );
     
     left_speed_demand = demand - forward_heading_output;
     right_speed_demand = demand + forward_heading_output;
-    Serial.print(", speed: ");
-    Serial.println( left_speed_demand );
+    Serial.print(", left speed: ");
+    Serial.print( left_speed_demand );
+    Serial.print(", right speed: ");
+    Serial.println( right_speed_demand );
   }
 }
 
 void walk(){
 
-  determine_angle();
+  getReadings();
 
   Serial.print( n_val );
   Serial.print(", ");
@@ -285,29 +272,23 @@ void walk(){
   SMALLEST  = n_val;
   FLAG_SMALLEST = NORTH; 
   //  dir = 0;  
+  
   MINUS90 = 0;
   if( SMALLEST > s_val ){
     Serial.println("s");
     SMALLEST = s_val;
-//    dir  = 180 + Pose.getThetaDegrees();
-
     FLAG_SMALLEST = SOUTH; 
   }
   
   if( SMALLEST > w_val){
     Serial.println("w");
     SMALLEST = w_val;
-//    dir = 90 + Pose.getThetaDegrees();
-//    if(dir > 180 ){dir = 175;}
-
     FLAG_SMALLEST = WEST; 
   }
 
   if( SMALLEST > e_val){
     Serial.println("e");
     SMALLEST = e_val;
-//    dir = -90 + Pose.getThetaDegrees();
-
     FLAG_SMALLEST = EAST; 
   }
   
@@ -315,9 +296,6 @@ void walk(){
  * FLAG is the heading direction of Romi
  * 
  */
-
-
-  
   if (FLAG_SMALLEST == NORTH){ //turn North/go forward
 
     if(FLAG == FORWARD){
@@ -393,9 +371,8 @@ void walk(){
       FLAG = UP;
       dir = 90; 
     }
-    
   }
-
+  
   Serial.print("Direction: ");
   Serial.println(dir);
 
@@ -408,9 +385,10 @@ void walk(){
     Serial.println("ROTATE");
     STATE = ROTATE;
   }
+  
 }
 
-int determine_angle(){
+int getReadings(){
   
   int x_index = Map.poseToIndex(Pose.getX(), MAP_X, MAP_RESOLUTION);
   int y_index = Map.poseToIndex(Pose.getY(), MAP_Y, MAP_RESOLUTION);
@@ -420,8 +398,6 @@ int determine_angle(){
   char w  = (char) EEPROM.read( ((x_index - 1) * MAP_RESOLUTION) + y_index ); 
   char e  = (char) EEPROM.read( ((x_index + 1) * MAP_RESOLUTION) + y_index ); 
 
-  
-  
   switch (FLAG){
     // -> -> -> -> -> -> -> -> -> -> -> -> 
     case 0: w_val  =  n;
@@ -455,10 +431,8 @@ int determine_angle(){
             Serial.println("down");
             break;
     default:  Serial.println("ERROR");
+    
   }
-  
-  
-
 }
 
 void rotate() {
@@ -475,54 +449,26 @@ void rotate() {
     output = HeadingControl.update( dir, theta );
   }
   
-  
-//  float condition = ;
-
-//  if (dir > 0){
-//    condition = (dir - Pose.getThetaDegrees() ) <= 0;
-//  }else{
-//    condition =  (Pose.getThetaDegrees() - dir  ) <= 0;
-//  }
-
-
   float error = HeadingControl.getError();
 
   if (error < 5){
-    Serial.print("Rotation finished: ");
-    Serial.println(Pose.getThetaDegrees());
+    Pose.update();
+    Serial.print(", right counts: ");
+    Serial.print(right_encoder_count);
+    Serial.print(", left counts: ");
+    Serial.println(left_encoder_count);
     stop_speed();
     delay(100);
     
     count = right_encoder_count + COUNT;
     STATE = FORWARD;
-    //STATE = WALK;
     
   } else {
 
-    left_speed_demand = -output;
+    left_speed_demand  = -output;
     right_speed_demand = output;
   }
-  
-//  if (condition) {
-//    Serial.println("Rotate finished");
-//    count = right_encoder_count + COUNT;
-//    STATE = FORWARD;
-//    //STATE = WALK;
-//    
-//  } else {
-//    
-//    float demand = -10;
-//
-//    left_speed_demand = demand;
-//    right_speed_demand = -demand;
-//    
-//  }
 }
-
-//int Mapper::poseToIndex(int x, int map_size, int resolution)
-//{
-//    return x / (map_size / resolution);
-//}
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
  * We have implemented a random walk behaviour for you
@@ -570,78 +516,21 @@ void doMovement() {
 
 }
 
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
- * This function groups up our sensor checks, and then
- * encodes into the map.  To get you started, we are 
- * simply placing a character into the map.  However,
- * you might want to look using a bitwise scheme to 
- * encode more information.  Take a look at mapping.h
- * for more information on how we are reading and
- * writing to eeprom memory.
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 void doMapping() {
   
-  // Read the IR Sensor and determine distance in
-  // mm.  Make sure you calibrate your own code!
-  // We threshold a reading between 40mm and 12mm.
-  // The rationale being:
-  // We can't trust very close readings or very far.
-  // ...but feel free to investigate this.
-
   //explored areas
   Map.updateMapFeature( (byte)'=', Pose.getY(), Pose.getX() );
   
   float distance = DistanceSensor.getDistanceInMM();
   if( distance < 40 && distance > 12 ) {
-
-    // We know the romi has the sensor mounted
-    // to the front of the robot.  Therefore, the
-    // sensor faces along Pose.Theta.
-    // We also add on the distance of the 
-    // sensor away from the centre of the robot.
+    
     distance += 80;
-
-
+    
     // Here we calculate the actual position of the obstacle we have detected
     float projected_x = Pose.getX() + ( distance * cos( Pose.getThetaRadians() ) );
     float projected_y = Pose.getY() + ( distance * sin( Pose.getThetaRadians() ) );
     //Map.updateMapFeature( (byte)'O', projected_y, projected_x );
-    
-    
   } 
-
-  // Check RFID scanner.
-  // Look inside RF_interface.h for more info.
-//  if( checkForRFID() ) {
-//
-//    // Add card to map encoding.  
-//    Map.updateMapFeature( (byte)'R', Pose.getY(), Pose.getX() );
-//
-//    // you can check the position reference and
-//    // bearing information of the RFID Card in 
-//    // the following way:
-//    // serialToBearing( rfid.serNum[0] );
-//    // serialToXPos( rfid.serNum[0] );
-//    // serialToYPos( rfid.serNum[0] );
-//    //
-//    // Note, that, you will need to set the x,y 
-//    // and bearing information in rfid.h for your
-//    // experiment setup.  For the experiment days,
-//    // we will tell you the serial number and x y 
-//    // bearing information for the cards in use.  
-//    
-//  } 
-
-  // Basic uncalibrated check for a line.
-  // Students can do better than this after CW1 ;)
-//  if( LineCentre.readRaw() > 580 ) {
-//      Map.updateMapFeature( (byte)'L', Pose.getY(), Pose.getX() );
-//  } 
-
-  // 
-  
 }
 
 void stop_speed(){
